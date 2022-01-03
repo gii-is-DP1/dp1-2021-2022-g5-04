3edf566c065d3467e7bc6efc3c65241cb7daf35e
Jose Ramon <jrae1112jrae@gmail.com>
2022-01-03 18:59:11 +0100

    .

--- src/main/java/org/springframework/samples/IdusMartii/repository/UserRepository.java
+++ src/main/java/org/springframework/samples/IdusMartii/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package org.springframework.samples.IdusMartii.repository;
+import org.springframework.samples.IdusMartii.enumerates.Role;
+import org.springframework.samples.IdusMartii.model.Player;
 import org.springframework.samples.IdusMartii.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -8,5 +13,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends  CrudRepository<User, String>{
     
+    @Query("SELECT u FROM Users u WHERE u.username = :username")
+    public User findByUsername(@Param("username") String username);
 	
 }
