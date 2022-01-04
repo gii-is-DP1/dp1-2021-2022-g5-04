db35199e8d7854ed7800945aec06b009f8cc20ed
Jose Ramon <jrae1112jrae@gmail.com>
2022-01-04 18:27:21 +0100

    arreglos auditoria

---
+++ src/main/java/org/springframework/samples/IdusMartii/web/AuditoriesController.java
@@ -0,0 +1,81 @@
+package org.springframework.samples.IdusMartii.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.IdusMartii.model.Achievement;
+import org.springframework.samples.IdusMartii.model.Match;
+import org.springframework.samples.IdusMartii.model.Player;
+import org.springframework.samples.IdusMartii.model.User;
+import org.springframework.samples.IdusMartii.service.MatchService;
+import org.springframework.samples.IdusMartii.service.PlayerService;
+import org.springframework.samples.IdusMartii.service.AchievementService;
+import org.springframework.samples.IdusMartii.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/auditories")	
+public class AuditoriesController {
+
+    @Autowired
+    private UserService userService;
+	
+    @Autowired
+    private PlayerService playerService;
+    
+    @Autowired
+    private MatchService matchService;
+    
+    @Autowired
+    private AchievementService AchievementService;
+    
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+    
+    @GetMapping()
+	public String auditoriasMenu(ModelMap modelMap) {
+		String vista = "auditories/auditoriasMenu";
+		
+		Iterable<User> users =  userService.findAll();
+		modelMap.addAttribute("users", users);
+		return vista;
+	}
+    
+    @GetMapping(path="/users")
+	public String auditoriasUsuario(ModelMap modelMap) {
+		String vista = "auditories/auditoriasUser";
+		
+		Iterable<User> users =  userService.findAll();
+		modelMap.addAttribute("users", users);
+		return vista;
+	} 
+    @GetMapping(path="/players")
+	public String auditoriasPlayer(ModelMap modelMap) {
+		String vista = "auditories/auditoriasPlayer";
+		
+		Iterable<Player> players =  playerService.findAll();
+		modelMap.addAttribute("players", players);
+		return vista;
+	}   
+    @GetMapping(path="/matches")
+	public String auditoriasMatch(ModelMap modelMap) {
+		String vista = "auditories/auditoriasMatch";
+		
+		Iterable<Match> matches =  matchService.findAll();
+		modelMap.addAttribute("matches", matches);
+		return vista;
+	}    @GetMapping(path="/achievements")
+	public String auditoriasAchievement(ModelMap modelMap) {
+		String vista = "auditories/auditoriasAchievement";
+		
+		Iterable<Achievement> achievements =  AchievementService.findAll();
+		modelMap.addAttribute("achievements", achievements);
+		return vista;
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/auditories/auditoriasUser.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page session="false" trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="IdusMartii" tagdir="/WEB-INF/tags" %>
+
+<IdusMartii:layout pageName="userAuditory">
+    <h2>Auditoria de usuarios</h2>
+
+    <table id="usersTable" class="table table-striped">
+        <thead>
+        <tr>
+            <th style="width: 200px;">Nombre</th>
+            <th style="width: 200px;">Fecha Creación</th>
+            <th style="width: 200px;">Creador</th>
+            <th style="width: 200px;">Última Modificación</th>
+            <th style="width: 200px;">Modificador</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${users}" var="user">
+            <tr>
+                <td>
+                	<c:out value="${user.username}"/>
+                </td>
+                <td>
+                	<c:out value="${user.createdDate}"/>
+                </td>
+                <td>
+                	<c:out value="${user.creator}"/>
+                </td>
+                <td>
+                    <c:out value="${user.lastModifiedDate}"/>
+                </td>
+                <td>
+                    <c:out value="${user.modifier}"/>
+                </td>
+
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</IdusMartii:layout>
