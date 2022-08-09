6441f4c64e82d04b3686c63944c394e8794ebb7d
JC <juamorper2@alum.us.es>
2022-08-09 19:15:09 +0200

    Eliminación de imports innecesarios

--- src/main/java/org/springframework/samples/IdusMartii/repository/MatchRepository.java
+++ src/main/java/org/springframework/samples/IdusMartii/repository/MatchRepository.java
@@ -3,7 +3,7 @@ package org.springframework.samples.IdusMartii.repository;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
